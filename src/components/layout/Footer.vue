63cf0836575a74c9fc24e34a21aa2a745517962c
visa4 <antonino.visalli@gmail.com>
2023-04-04 17:54:21 +0200

    Fix vlayout

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,6 +1,11 @@
 <template>
     <Toolbar id="p-footer" class="p-footer">
-     
+      <template #start>
+        <slot name="start"></slot>
+      </template>
+      <template #end>
+        <slot name="end"></slot>
+      </template>
     </Toolbar>
 </template>
 
@@ -20,9 +25,9 @@ export default {
 </script>
 
 <style>
-.p-footer {
+  .p-footer {
 
-      height: 5rem;
+      height: 7rem;
       width: 100%;
       padding: 0 2rem;
       transition: left .2s;
@@ -31,6 +36,6 @@ export default {
       /* box-shadow: 0 3px 5px #00000005; */
       border-radius: 0 !important;
       border: 0 !important;
-      background-color: black !important;
+      background-color: var(--ekocasa-black) !important;
   }
 </style>
--- src/template/Landing.vue
+++ src/template/Landing.vue
@@ -5,20 +5,38 @@ import Button from 'primevue/button';
 import HeaderCollapse from '../components/layout/HeaderCollapse.vue';
 import ImageDescriptor from '../components/media/ImageDescriptor.vue';
 import Footer from '../components/layout/Footer.vue';
+import { GoogleMap, Marker } from 'vue3-google-map';
 
 export default {
   name: "App",
   components: {
     Button,
+    GoogleMap,
     HeaderCollapse,
+    Marker,
     ImageDescriptor,
     Footer
+  }, 
+  data() {
+    return {
+      center: {lat: 45.6571708, lng: 9.01780280},
+      markers: [
+        {
+          position: {
+            lat: 45.6571708, lng: 9.0178028
+          },
+        }
+      ]
+    }
   }
 };
 </script>
 
 <template>
     <HeaderCollapse>
+        <template #start>
+            <div class="logo"></div>
+        </template>
         <template #end>
             <a href="mailto:a.visalli@ekocasa.it?cc=s.pizzino@ekocasa.it&subject=Informazioni&body=Buongiorno ekocasa potrei avere maggiori informazioni in merito a..." target="_blank" class="btn-inv">
                 <Button icon="pi pi-send" rounded severity="mail"/>
@@ -33,7 +51,17 @@ export default {
             </div>
         </template>
     </HeaderCollapse>
-    <section class="column product">
+    <section>
+        <div class="mission">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.    
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.    
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.    
+        
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.    
+        
+        </div>
+    </section>
+    <section class="column product bc-grey">
         <ImageDescriptor img-url="img/card/pavimenti.webp">
             <template #out>Pavimenti</template>
             <template #inside>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in</template>
@@ -59,18 +87,54 @@ export default {
             <template #inside>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in</template> 
         </ImageDescriptor>
     </section>
+
     <section>
-      
+        <GoogleMap api-key="AIzaSyBnK6JUSmdnAM_QRBHKmfmwDvTYgNxdstI" style="width: 100%; height: 500px" :center="center" :zoom="16">
+            <Marker :options="{ position: center }"  />
+        </GoogleMap>
     </section>
-    <Footer></Footer>
+    <section style="background-color:grey;">
+      fornitori
+    </section>
+    <Footer>
+        <template #start>
+            <div class="footer-data">
+                <div>Ekocasa Srl</div>
+                <div>Piva: 04038370138</div>
+                <div>Via isonzo 5 Turate (VA)</div>
+            </div>
+          
+        </template>
+        <template #end>
+            <div class="pi pi-google"></div>
+            <div class="pi pi-facebook"></div>
+            <div class="pi pi-linkedin"></div>
+        </template>
+    </Footer>
 </template>
 
 <style scoped>
+
+    div.pi {
+        margin-right: 20px;
+    }
+
+    .pi {
+        color: var(--ekocasa-green);
+        font-size: 30px;
+    }
+
+    video {
+        max-width: 100%;
+        width: 100%;
+        height: 600px;
+    }
+
     .header-content {
         font-size: 32px;
         display: flex;
 
-        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+        text-shadow: -1px 0 var(--ekocasa-black), 0 1px var(--ekocasa-black), 1px 0 var(--ekocasa-black), 0 -1px var(--ekocasa-black);
 
         font-family: sans; color: #FFFFFF;
         width: 80%;
@@ -80,9 +144,17 @@ export default {
     }
 
     section {
-        padding: 40px 0;
+        padding: 80px 0;
         display: flex;
         justify-content: center;
+    }
+
+    .bc-grey {
+        background-color: #CBCBCB;
+    }
+
+    .mission {
+        width: 70%;
     }
 
     .column {
@@ -100,28 +172,26 @@ export default {
         color: transparent
     }
        
-    .product-title {
-        top: -150px;
-        font-size: 22px;
-        margin-left: 100px;
-        color: #ffffff;
-        position: relative;
-        font-weight: 700;
-        width: 150px;
+    .logo {
+        height: 64px;
+        width: 64px;
+        background-size: contain;
+
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-image: url("img/header/logo.png");
     }
 
-    .card-img {
-        position: relative;
-        top: -86px;
-        border-radius: 100%;
-        height: 60px;
-        width:  60px;
+    .footer-data {
+        font-size: 20px;
     }
+
     /**
      * DEEP IMAGE_DESCRIPTOR
      */
     :deep(.p-image-descriptor) {
         margin-right: 8px;
+        margin-bottom: 8px;
         display: flex;
         align-items: center;
         font-size: 36px;
@@ -145,24 +215,26 @@ export default {
      */
     :deep(.p-button) {
         margin-right: 10px;
-        height: 4rem !important;
-        width: 4rem !important;
+        height: 3rem !important;
+        width: 3rem !important;
     }
 
     :deep(.p-button .pi) {
-        font-size: 2rem;
+        font-size: 30px;
     }
 
     @media screen and (max-width: 600px) {
         .p-image-descriptor {
             width: 100%  !important;
             margin-right: 0;
+            margin-bottom: 0px;
         }
     }
 
     @media only screen and (min-width: 601px) and (max-width: 1200px) {
         .p-image-descriptor {
             width: 48%  !important;
+            margin-bottom: 8px;
         }
         .p-image-descriptor:nth-child(even) {
             margin-right: 0;
@@ -172,9 +244,10 @@ export default {
     @media only screen and (min-width: 1201px) and (max-width: 1600px) {
         .p-image-descriptor {
             width: 32%  !important;
+            margin-bottom: 8px;
         }
 
-        .p-image-descriptor:nth-child(even) {
+        .p-image-descriptor:nth-child(3n) {
             margin-right: 0;
         }
     }
